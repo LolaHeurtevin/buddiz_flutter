b2846b1ba51ac68a0edee8f79a59a19ea2a7ac54
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2025-11-20 16:20:52 +0100

    Ajout d'une appbar sur toutes les pages sauf l'accueil

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -27,6 +27,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MainLayout(
+      showAppBar: false,
       child: FutureBuilder<List<Activity>>(
         future: futureActivities,
         builder: (context, snapshot) {
--- lib/views/widgets/main_layout.dart
+++ lib/views/widgets/main_layout.dart
@@ -3,15 +3,29 @@ import 'bottom_navigation_bar.dart';
 
 class MainLayout extends StatelessWidget {
   final Widget child;
+  final bool showAppBar;
 
-  const MainLayout({super.key, required this.child});
+  const MainLayout({
+    super.key,
+    required this.child,
+    this.showAppBar = true,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: SafeArea(
-      child: child,
-    ),
+      appBar: showAppBar
+          ? AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: BackButton(
+          color: Colors.black,
+        ),
+      )
+          : null,
+      body: SafeArea(
+        child: child,
+      ),
       bottomNavigationBar: const CustomNavigationBar(),
     );
   }
